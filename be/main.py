6df59bd860275139bf7865d507892fcb36086d7f
MDSK <oacgodkl21@gmail.com>
2023-07-04 22:53:08 +0800

    test: Test generate short url
    - 測試亂數產生功能與新增 post API

--- be/main.py
+++ be/main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import secrets
 
 app = FastAPI()
+
+# start uvicorn server:
+# uvicorn main:app --reload
+
+
+class URL(BaseModel):
+    url: str
+
+
+def generate_short_url():
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+    short_url = "".join(secrets.choice(alphabet) for _ in range(6))
+    return short_url
 
 
 @app.get("/")
 def read_main():
-    return {"Hello" : "World"}
+    return {"Hello": "World"}
+
+
+@app.post("/shorten")
+def shorten_url(url: URL):
+    # return url
+    short_url = generate_short_url()
+    return {"original_url": url.url, "short_url": short_url}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
